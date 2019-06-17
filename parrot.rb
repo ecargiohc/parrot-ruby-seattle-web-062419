dcdb198629e5bafe2a55200e1cce1e62cb6fb192
Grace Colette <ilovegrace09@gmail.com>
2019-06-17 23:30:22 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
-def parrot("Squawk!")
-  puts "#{Squawk!}"
-  return "#{Squawk!}"
+def parrot(yell="Squawk!")
+  puts "#{yell}"
+  return "#{yell}"
 end# Create method `parrot` that outputs a given phrase and
 # returns the phrase
